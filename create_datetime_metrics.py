bdd28f7ae9e484432c57bb0a1aed632c6ab83428
psilosanctum <c2cypher@protonmail.com>
2024-03-30 00:51:27 -0500

    perform calcs at the daily level and aggregate up

--- create_datetime_metrics.py
+++ create_datetime_metrics.py
@@ -2,30 +2,26 @@ import pandas as pd
 import time
 pd.set_option('display.max_columns', None)
 
-wkly_date_range = pd.date_range('2024-01-01', '2026-12-31', freq ='W').tolist()
 daily_dates = pd.date_range('2024-01-01', '2026-12-31', freq ='D').tolist()
-# print(daily_dates)
-convert_dt_unix = []
-convert_dt_str = []
-datetime_format = []
-for i in wkly_date_range:
-    datetime_format.append(i)
-    str_dt = pd.to_datetime(i).strftime("%Y-%m-%d")
-    unix = (time.mktime(i.timetuple()))
-    convert_dt_unix.append(unix)
-    convert_dt_str.append(str_dt)
-
-time_df = pd.DataFrame()
-time_df['unix'] = convert_dt_unix
-time_df['unix'] = time_df['unix'].astype(int)
-time_df['str'] = convert_dt_str
-time_df['datetime_obj'] = datetime_format
-time_df['week_number'] = time_df['datetime_obj'].dt.isocalendar().week
-
 week_day = []
-for i in time_df['str']:
-    converted = pd.Timestamp(i).day_of_week
-    week_day.append(converted)
-# print(week_day)
-# time_df['day_number'] = pd.Timestamp(time_df['str']).day_of_week
-# print(time_df)
+week_number = []
+daily_date_str = []
+convert_unix = []
+for i in daily_dates:
+    str_dt = pd.to_datetime(i).strftime("%Y-%m-%d")
+    converted_day = pd.Timestamp(i).day_of_week
+    week_number.append(i)
+    daily_date_str.append(str_dt)
+    week_day.append(converted_day)
+    unix = (time.mktime(i.timetuple()))
+    convert_unix.append(unix)
+
+daily_dates_df = pd.DataFrame()
+daily_dates_df['dates'] = daily_date_str
+daily_dates_df['day_of_week'] = week_day
+daily_dates_df['datetime_obj'] = week_number
+daily_dates_df['week_number'] = daily_dates_df['datetime_obj'].dt.isocalendar().week
+daily_dates_df['unix'] = convert_unix
+daily_dates_df['unix'] = daily_dates_df['unix'].astype(int)
+daily_dates_df = daily_dates_df[['unix', 'dates', 'datetime_obj', 'week_number', 'day_of_week']]
+print(daily_dates_df)
